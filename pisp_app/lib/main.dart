37a47a3ca959d4e8d23c232ca10593caff1c6a25
MahidharBandaru <mahi.mahidhar2@gmail.com>
2020-06-16 11:20:27 +0530

    Setup routing module

--- pisp_app/lib/main.dart
+++ pisp_app/lib/main.dart
@@ -1,67 +1,86 @@
 import 'package:flutter/material.dart';
+import 'route_generator.dart';
 
-void main() {
-  runApp(MyApp());
-}
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
         primarySwatch: Colors.blue,
-        visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      // Initially display FirstPage
+      home: FirstPage(),
+      initialRoute: '/',
+      onGenerateRoute: RouteGenerator.generateRoute,
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  final String title;
-
+class FirstPage extends StatelessWidget {
   @override
-  _MyHomePageState createState() => _MyHomePageState();
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Routing App'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: <Widget>[
+            Text(
+              'First Page',
+              style: TextStyle(fontSize: 50),
+            ),
+            RaisedButton(
+              child: Text('Go to second'),
+              onPressed: () {
+                Navigator.of(context).pushNamed(
+                  '/second',
+                  arguments: 'Hey',
+                );
+              },
+            )
+          ],
+        ),
+      ),
+    );
+  }
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class SecondPage extends StatelessWidget {
+  // This is a String for the sake of an example.
+  // You can use any type you want.
+  final String data;
 
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
+  SecondPage({
+    Key key,
+    @required this.data,
+  }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(widget.title),
+        title: Text('Routing App'),
       ),
       body: Center(
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
+          mainAxisSize: MainAxisSize.min,
           children: <Widget>[
             Text(
-              'You have pushed the button this many times:',
+              'Second Page',
+              style: TextStyle(fontSize: 50),
             ),
             Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
+              data,
+              style: TextStyle(fontSize: 20),
             ),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }

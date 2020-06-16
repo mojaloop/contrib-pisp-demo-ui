bfdcce323336440208b770ffb2557bfbd4529ef6
MahidharBandaru <mahi.mahidhar2@gmail.com>
2020-06-16 15:30:35 +0530

    Add Dashboard layout

---
+++ pisp_app/lib/Screens/Dashboard.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+class Dashboard extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: DefaultTabController(
+        length: 2,
+        child: Scaffold(
+          appBar: AppBar(
+            bottom: TabBar(
+              tabs: [
+                Tab(text: 'My Accounts'),
+                Tab(text: 'Pay Now'),
+              ],
+            ),
+            title: Center(child: Text('Dashboard')),
+          ),
+          body: TabBarView(
+            children: [
+              Icon(Icons.directions_car),
+              Icon(Icons.directions_transit),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}

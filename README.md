da2a47bc717c8a046f80281b226a125b3626d8a4
Mahidhar Bandaru <mahi.mahidhar2@gmail.com>
2020-07-06 13:17:50 +0530

    Payment flow (#2)
    * Add setup instructions to README.
    
    * Add support for logging
    
    * Setup routing module
    
    * Add page to enter phone number
    
    * Rename SignUpPage and move it out of main to its own file
    
    * Add Dashboard layout
    
    * Add global error page
    
    * Add mockdata for Account
    
    * Add transaction success page
    
    * Add experimental transaction detail page
    
    * Add new payment screen
    
    * Add fingerprint auth support
    
    * Add files for all screens. Add Transaction mockdata. Modified Dashboard to point to correct routes. Add layout for account details page.
    
    * Add AccountDashboard, Transaction Details, FindPayee screens. Changed NewPayment Theming
    
    * Rename Account screen to AccountDetails
    
    * Move phone number state to parent
    
    * Change Account Dummy Data
    
    * Fix FindPayee
    
    * Pass data to TransactionDetails and use it. Add TransactionAmount Screen
    
    * Change TransactionSuccess Screen gradient color to green and change text
    
    * Refactor NewPayment
    
    * Rename FindPayee -> LookupPayee
    
    * Add lint configuration
    
    * Cleanup. Remove unused screens, pass static analysis checks
    
    * Steven code review - Part 1
    
    * Change root project folder
    
    * Remove starter app test
    
    * Remove trailing commas
    
    * Change clipoval to floatingActionButton

---
+++ README.md
@@ -0,0 +1,12 @@
+# PISP App Flutter
+
+A demo app to do payment initiation using mojaloop.
+
+**To setup flutter app on android studio**:
+
+1. [Install](https://flutter.dev/docs/get-started/install) the flutter sdk.
+2. [Install](https://developer.android.com/studio/install) Andriod Studio.
+2. Install flutter and dart plugin on android studio. Go to File->Settings->Plugins and install flutter plugin.
+3. Clone the repository and open the pisp_app from android studio
+4. Go to settings -> flutter and add the path to flutter SDK if not already present.
+5. Connect device/ emulator and run the app.
---
+++ lib/main.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+import 'package:pispapp/Screens/dashboard.dart';
+import 'package:pispapp/Screens/error.dart';
+import 'route_generator.dart';
+
+void main() {
+  print('starting app');
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(
+    BuildContext context,
+  ) {
+    return MaterialApp(
+      title: 'Mojapay',
+      theme: ThemeData(
+        primarySwatch: Colors.green,
+      ),
+      builder: (
+        BuildContext context,
+        Widget widget,
+      ) {
+        ErrorWidget.builder = (
+          FlutterErrorDetails errorDetails,
+        ) {
+          return Error.genericError();
+        };
+        return widget;
+      },
+      // Initially display FirstPage
+      home: Dashboard(),
+      initialRoute: '/',
+      onGenerateRoute: RouteGenerator.generateRoute,
+    );
+  }
+}

9c8da9b4d6dc7f40b0e405048116d363f3420f4e
Mahidhar <mahi.mahidhar2@gmail.com>
2020-07-08 13:25:37 +0530

    Add modal sheets for menus in account_dashboard

--- lib/Screens/account_dashboard.dart
+++ lib/Screens/account_dashboard.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:pispapp/MockData/account.dart';
+import 'package:pispapp/MockData/transaction.dart';
 import 'package:pispapp/theme/light_color.dart';
 import 'package:pispapp/widgets/balance_card.dart';
 import 'package:pispapp/widgets/title_text.dart';
@@ -24,6 +25,84 @@ class _AccountDashboardState extends State<AccountDashboard> {
           (Account element) => element.linked,
         )
         .toList();
+    account = accounts[0];
+  }
+
+  Widget _getAccountTile(BuildContext context, Account acc) {
+    return Padding(
+      padding: const EdgeInsets.fromLTRB(20, 0, 20, 0),
+      child: ListTile(
+        onTap: () {
+          setState(() {
+            account = acc;
+          });
+        },
+        leading: const CircleAvatar(),
+        contentPadding: const EdgeInsets.symmetric(),
+        title: TitleText(
+          text: acc.alias,
+          fontSize: 14,
+        ),
+        subtitle: Text(account.accountNumber),
+        trailing: Container(
+          height: 30,
+          width: 60,
+          alignment: Alignment.center,
+          decoration: BoxDecoration(
+            color: LightColor.lightGrey,
+            borderRadius: const BorderRadius.all(
+              Radius.circular(
+                10,
+              ),
+            ),
+          ),
+          child: Text(
+            acc.balance,
+            style: GoogleFonts.muli(
+              fontSize: 12,
+              fontWeight: FontWeight.bold,
+              color: LightColor.navyBlue2,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  void _showAccountChoosingBottomSheet() {
+    List<Widget> columnWidgets = [
+      Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: const TitleText(text: 'Accounts'),
+      ),
+      const Divider(
+        height: 20,
+      ),
+    ];
+    List<Widget> accountButtons =
+        accounts.map((e) => _getAccountTile(context, e)).toList();
+
+    columnWidgets.addAll(accountButtons);
+
+    showModalBottomSheet<void>(
+      context: context,
+      builder: (context) {
+        return Container(
+          height: 350.0,
+          color: Color(0xFF737373),
+          child: Container(
+            decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.only(
+                    topLeft: const Radius.circular(20.0),
+                    topRight: const Radius.circular(20.0))),
+            child: ListView(
+              children: columnWidgets,
+            ),
+          ),
+        );
+      },
+    );
   }
 
   Widget _appBar() {
@@ -31,14 +110,18 @@ class _AccountDashboardState extends State<AccountDashboard> {
       children: <Widget>[
         const CircleAvatar(),
         const SizedBox(width: 15),
-        TitleText(text: accounts[0].alias),
+        TitleText(text: account.alias),
         const Expanded(
           child: SizedBox(),
         ),
-        Icon(
-          Icons.short_text,
-          color: Theme.of(context).iconTheme.color,
-        )
+        GestureDetector(
+            onTap: () {
+              _showAccountChoosingBottomSheet();
+            },
+            child: Icon(
+              Icons.short_text,
+              color: Theme.of(context).iconTheme.color,
+            )),
       ],
     );
   }
@@ -92,24 +175,154 @@ class _AccountDashboardState extends State<AccountDashboard> {
   }
 
   Widget _transactionList() {
+    List<Transaction> debitTransactionList =
+        getDebitTransactions(account.accountNumber);
     return Column(
-      children: <Widget>[
-        _transaction('Mark Doe', '23 Feb 2020', '20'),
-        _transaction('John Doe', '25 Feb 2020', '20'),
-        _transaction('Flight Ticket', '03 Mar 2020', '20'),
-        _transaction('Flight Ticket', '03 Mar 2020', '-20'),
-      ],
+      children: debitTransactionList.map((e) => _transaction(e)).toList(),
     );
   }
 
-  Widget _transaction(String text, String time, String amount) {
-    var icon = (amount[0] == '-')
-        ? Icons.keyboard_arrow_right
-        : Icons.keyboard_arrow_left;
-    final color =
-        (amount[0] != '-') ? const Color(0xff4caf50) : const Color(0xffd50000);
+  void _showTransactionDetailBottomSheet(Transaction t) {
+    IconData icon = t.status == Status.SUCCESSFUL ? Icons.check_circle_outline : (
+      t.status == Status.PENDING ? Icons.assignment_late : Icons.cancel
+    );
+    Color textColor = t.status == Status.SUCCESSFUL ? Colors.green : (
+      t.status == Status.PENDING ? Colors.yellow : Colors.red
+    );
+    String text = t.status == Status.SUCCESSFUL ? 'Successful' : (
+      t.status == Status.PENDING ? 'Pending' : 'Error'
+    );
+    showModalBottomSheet<dynamic>(
+      isScrollControlled: true,
+      context: context,
+      builder: (context) {
+        return Container(
+          color: Color(0xFF737373),
+          child: Wrap(
+            children: <Widget>[
+              Container(
+                decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.only(
+                        topLeft: const Radius.circular(20.0),
+                        topRight: const Radius.circular(20.0))),
+                child: Column(
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.all(20.0),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: <Widget>[
+                          Column(
+                            children: <Widget>[
+                              Icon(
+                                icon,
+                                size: 40,
+                              ),
+                              SizedBox(height: 20),
+                              TitleText(
+                                text: text,
+                                fontSize: 28,
+                                color: textColor,
+                              ),
+                              SizedBox(height: 20),
+                              Padding(
+                                padding: const EdgeInsets.all(8.0),
+                                child: ButtonTheme(
+                                  minWidth:
+                                      MediaQuery.of(context).size.width * 0.6,
+                                  height: 70.0,
+                                  child: RaisedButton(
+                                    shape: RoundedRectangleBorder(
+                                        borderRadius:
+                                            BorderRadius.circular(18.0),
+                                        side: BorderSide(color: Colors.grey)),
+                                    onPressed: () {},
+                                    color: LightColor.navyBlue2,
+                                    textColor: Colors.white,
+                                    child: Text('${t.amount} \$',
+                                        style: TextStyle(fontSize: 28)),
+                                  ),
+                                ),
+                              ),
+                            ],
+                          ),
+                        ],
+                      ),
+                    ),
+                    Divider(
+                      height: 8,
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.fromLTRB(
+                        20.0,
+                        0,
+                        20,
+                        0,
+                      ),
+                      child: ListTile(
+                        contentPadding: const EdgeInsets.symmetric(),
+                        title: TitleText(
+                          text: 'From',
+                          fontSize: 14,
+                        ),
+                        subtitle: Text(account.accountNumber),
+                        trailing: TitleText(text: account.name, fontSize: 14),
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.fromLTRB(
+                        20.0,
+                        0,
+                        20,
+                        0,
+                      ),
+                      child: ListTile(
+                        contentPadding: const EdgeInsets.symmetric(),
+                        title: TitleText(
+                          text: 'To',
+                          fontSize: 14,
+                        ),
+                        subtitle: Text(t.to),
+                        trailing: TitleText(text: t.payeeName, fontSize: 14),
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.fromLTRB(
+                        20.0,
+                        0,
+                        20,
+                        0,
+                      ),
+                      child: ListTile(
+                        contentPadding: const EdgeInsets.symmetric(),
+                        title: TitleText(
+                          text: 'Date',
+                          fontSize: 14,
+                        ),
+                        // subtitle: Text(account.accountNumber),
+                        trailing: TitleText(text: t.date, fontSize: 14),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        );
+      },
+    );
+  }
+
+  Widget _transaction(Transaction t) {
+    var icon = Icons.keyboard_arrow_right;
+    final color = const Color(0xffd50000);
 
     return ListTile(
+      onTap: () {
+        _showTransactionDetailBottomSheet(t);
+      },
       leading: Container(
         height: 50,
         width: 50,
@@ -121,24 +334,29 @@ class _AccountDashboardState extends State<AccountDashboard> {
       ),
       contentPadding: const EdgeInsets.symmetric(),
       title: TitleText(
-        text: text,
+        text: t.payeeName,
         fontSize: 14,
       ),
-      subtitle: Text(time),
+      subtitle: Text(t.date),
       trailing: Container(
         height: 30,
         width: 60,
         alignment: Alignment.center,
         decoration: BoxDecoration(
           color: LightColor.lightGrey,
-          borderRadius: const BorderRadius.all(Radius.circular(10)),
+          borderRadius: const BorderRadius.all(
+            Radius.circular(
+              10,
+            ),
+          ),
         ),
         child: Text(
-          amount,
+          t.amount,
           style: GoogleFonts.muli(
-              fontSize: 12,
-              fontWeight: FontWeight.bold,
-              color: LightColor.navyBlue2),
+            fontSize: 12,
+            fontWeight: FontWeight.bold,
+            color: LightColor.navyBlue2,
+          ),
         ),
       ),
     );
@@ -162,7 +380,7 @@ class _AccountDashboardState extends State<AccountDashboard> {
                 const SizedBox(
                   height: 20,
                 ),
-                BalanceCard(account: accounts[0]),
+                BalanceCard(account: account),
                 const SizedBox(
                   height: 50,
                 ),

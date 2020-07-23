ec5720ebef0459244b2dce00ebadfa31b3085679
Mahidhar <mahi.mahidhar2@gmail.com>
2020-07-23 11:06:51 +0530

    Fix lint errors

--- lib/models/model.dart
+++ lib/models/model.dart
@@ -1,7 +1,6 @@
 abstract class Model {
+  // ignore: avoid_unused_constructor_parameters
   Model.fromJson(Map<String, dynamic> json);
 
-
   Map<String, dynamic> toJson();
-
 }
--- lib/routes/app_pages.dart
+++ lib/routes/app_pages.dart
@@ -5,13 +5,20 @@ import 'package:pispapp/ui/pages/splash.dart';
 
 part './app_routes.dart';
 
+// ignore: avoid_classes_with_only_static_members
 abstract class AppPages {
-
   static final pages = [
-    GetPage(name: Routes.SPLASH, page:()=> SplashScreen(),),
-    GetPage(name: Routes.LOGIN, page:()=> Login(),),
-    GetPage(name: Routes.PROFILE, page:()=> Profile(),),
-
-
+    GetPage(
+      name: Routes.SPLASH,
+      page: () => SplashScreen(),
+    ),
+    GetPage(
+      name: Routes.LOGIN,
+      page: () => Login(),
+    ),
+    GetPage(
+      name: Routes.PROFILE,
+      page: () => Profile(),
+    ),
   ];
 }

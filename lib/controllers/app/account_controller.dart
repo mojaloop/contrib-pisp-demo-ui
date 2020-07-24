affb4012dade474df9664096186f19a33db56c01
Mahidhar <mahi.mahidhar2@gmail.com>
2020-07-24 15:26:42 +0530

    Add AccountController to app state

---
+++ lib/controllers/app/account_controller.dart
@@ -0,0 +1,22 @@
+import 'package:get/get.dart';
+import 'package:pispapp/controllers/ephemeral/account_dashboard_controller.dart';
+import 'package:pispapp/models/account.dart';
+import 'package:pispapp/repositories/interfaces/i_account_repository.dart';
+import 'package:pispapp/utils/log_printer.dart';
+
+class AccountController extends GetxController {
+  AccountController(this._accountRepository);
+  var accounts = <Account>[].obs;
+
+  IAccountRepository _accountRepository;
+
+  void getAllLinkedAccounts() {
+    accounts.value = _accountRepository.getUserAccounts();
+    final logger = getLogger('printing size');
+    logger.e(accounts.value.length);
+    Get.find<AccountDashboardController>().setSelectedAccount(accounts.value[0]);
+    
+  }
+
+
+}

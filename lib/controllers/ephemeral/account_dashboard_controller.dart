b0c8df381aaaa922f902320ee11553a6a1e7ba50
Mahidhar <mahi.mahidhar2@gmail.com>
2020-07-24 15:28:18 +0530

    Add AccountDashboard ui page and the controller for it

---
+++ lib/controllers/ephemeral/account_dashboard_controller.dart
@@ -0,0 +1,39 @@
+import 'package:get/get.dart';
+import 'package:meta/meta.dart';
+import 'package:pispapp/controllers/app/account_controller.dart';
+import 'package:pispapp/models/account.dart';
+import 'package:pispapp/models/transaction.dart';
+import 'package:pispapp/repositories/interfaces/i_transaction_repository.dart';
+import 'package:pispapp/utils/log_printer.dart';
+
+class AccountDashboardController extends GetxController {
+  AccountDashboardController(this._transactionRepo);
+  Account selectedAccount;
+  List<Transaction> transactionList = <Transaction>[];
+
+  ITransactionRepository _transactionRepo;
+
+  void refreshAll() {}
+
+  void onAccountTileTap(Account acc) {
+    setSelectedAccount(acc);
+  }
+
+  void setSelectedAccount(Account acc) {
+    selectedAccount = acc;
+    update();
+    updateTransactions();
+  }
+
+  void updateTransactions() {
+    final logger = getLogger('AccountDashboardController');
+    logger.e('getting transactions');
+    transactionList = _transactionRepo.getTransactions(selectedAccount.accountNumber);
+    update();
+  }
+
+  void refreshAccounts() {
+    Get.find<AccountController>().getAllLinkedAccounts();
+    // TODO(MahidharBandaru): Handle empty case;
+  }
+}
---
+++ lib/ui/widgets/icon.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class CustomIcon extends StatelessWidget {
+  CustomIcon(this.icon, this.text, this.onTap);
+  final IconData icon;
+  final String text;
+  final void Function() onTap;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: <Widget>[
+        GestureDetector(
+          onTap: onTap,
+          child: Container(
+            height: 80,
+            width: 80,
+            margin: const EdgeInsets.symmetric(
+              vertical: 10,
+            ),
+            decoration: const BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.all(
+                Radius.circular(
+                  20,
+                ),
+              ),
+              boxShadow: [
+                BoxShadow(
+                  color: Color(0xfff3f3f3),
+                  offset: Offset(
+                    5,
+                    5,
+                  ),
+                  blurRadius: 10,
+                )
+              ],
+            ),
+            child: Icon(icon),
+          ),
+        ),
+        Text(
+          text,
+          style: GoogleFonts.muli(
+            fontSize: 15,
+            fontWeight: FontWeight.w600,
+            color: const Color(
+              0xff76797e,
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}

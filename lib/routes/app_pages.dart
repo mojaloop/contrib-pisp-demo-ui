895fd6b7a8e946d28207a5e9f9e3b49a0fc13a43
Mahidhar <mahi.mahidhar2@gmail.com>
2020-07-28 12:18:50 +0530

    Use bidings in routes. Add new routes for transfer

--- lib/routes/app_pages.dart
+++ lib/routes/app_pages.dart
@@ -1,7 +1,15 @@
 import 'package:get/get.dart';
+import 'package:pispapp/bindings/login_binding.dart';
+import 'package:pispapp/bindings/lookup_payee_bindings.dart';
+import 'package:pispapp/bindings/payment_details_binding.dart';
+import 'package:pispapp/bindings/payment_finalize_bindings.dart';
+import 'package:pispapp/bindings/splash_binding.dart';
+import 'package:pispapp/controllers/ephemeral/account_dashboard_controller.dart';
 import 'package:pispapp/ui/pages/dashboard.dart';
 import 'package:pispapp/ui/pages/login.dart';
-import 'package:pispapp/ui/pages/profile.dart';
+import 'package:pispapp/ui/pages/lookup_payee.dart';
+import 'package:pispapp/ui/pages/payment_details.dart';
+import 'package:pispapp/ui/pages/payment_finalize.dart';
 import 'package:pispapp/ui/pages/splash.dart';
 
 part './app_routes.dart';
@@ -12,18 +20,36 @@ abstract class AppPages {
     GetPage(
       name: Routes.SPLASH,
       page: () => SplashScreen(),
+      binding: SplashBinding(),
     ),
     GetPage(
       name: Routes.LOGIN,
       page: () => Login(),
-    ),
-    GetPage(
-      name: Routes.PROFILE,
-      page: () => Profile(),
+      binding: LoginBinding(),
     ),
     GetPage(
       name: Routes.DASHBOARD,
-      page: () => Dashboard(),
+      page: ()  {
+        Get.find<AccountDashboardController>().onRefresh();
+        return Dashboard();
+      }
+      // binding: DashboardBinding(),
+
+    ),
+    GetPage(
+      name: Routes.TRANSFER_LOOKUP,
+      page: () => LookupPayee(),
+      binding: LookupPayeeBinding(),
+    ),
+    GetPage(
+      name: Routes.TRANSFER_FINALIZE,
+      page: () => PaymentFinalize(),
+      binding: PaymentFinalizeBinding(),
+    ),
+    GetPage(
+      name: Routes.TRANSGER_DETAILS,
+      page: () => PaymentDetails(),
+      binding: PaymentDetailsBinding(),
     ),
   ];
 }

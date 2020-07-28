5b7943f21a0df2d587666d731ae4bff113323baf
Mahidhar <mahi.mahidhar2@gmail.com>
2020-07-28 12:12:09 +0530

    Add lookup payee page

---
+++ lib/controllers/ephemeral/lookup_payee_controller.dart
@@ -0,0 +1,29 @@
+import 'package:get/get.dart';
+import 'package:pispapp/bindings/payment_finalize_bindings.dart';
+import 'package:pispapp/controllers/ephemeral/payment_initiate_controller.dart';
+import 'package:pispapp/models/account.dart';
+import 'package:pispapp/repositories/interfaces/i_account_repository.dart';
+import 'package:pispapp/ui/pages/payment_finalize.dart';
+
+class LookupPayeeController extends GetxController {
+  LookupPayeeController(this._accountRepository);
+
+  List<Account> payeeAccounts;
+  IAccountRepository _accountRepository;
+  Account payeeAccount;
+
+  @override
+  void onInit() {
+    final String phoneNumber =
+        Get.find<PaymentInitiateController>().phoneIsoCode +
+            Get.find<PaymentInitiateController>().phoneNumber;
+    payeeAccounts = _accountRepository.getPayeeAccountsByPhone(phoneNumber);
+    super.onInit();
+    update();
+  }
+
+  void onTapPayertile(Account account) {
+    payeeAccount = account;
+    Get.toNamed<dynamic>('/transfer/finalize');
+  }
+}

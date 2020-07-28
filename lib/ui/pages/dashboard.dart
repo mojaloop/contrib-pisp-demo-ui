e29470cfb632c55e22cec71df7b8ece1e07f31a0
Mahidhar <mahi.mahidhar2@gmail.com>
2020-07-28 12:10:31 +0530

    Change dashboard and add binding for controller lifecycle management

--- lib/ui/pages/dashboard.dart
+++ lib/ui/pages/dashboard.dart
@@ -1,55 +1,51 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:pispapp/controllers/ephemeral/dashboard_controller.dart';
+import 'package:pispapp/ui/pages/payment_initiate.dart';
 import 'package:pispapp/ui/theme/light_theme.dart';
 
 class Dashboard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Container(
-        child: GetBuilder<DashboardController>(
-          builder: (_) {
-            return Scaffold(
-              body: Center(
-                child: _.widgetOptions.elementAt(_.selectedIndex),
+    return GetBuilder<DashboardController>(
+      builder: (_) {
+        return Scaffold(
+          resizeToAvoidBottomPadding: false,
+          body: _.widgetOptions.elementAt(_.selectedIndex),
+          bottomNavigationBar: BottomNavigationBar(
+            items: <BottomNavigationBarItem>[
+              BottomNavigationBarItem(
+                icon: Icon(Icons.account_balance),
+                title: const Text(
+                  'Accounts',
+                ),
               ),
-              bottomNavigationBar: BottomNavigationBar(
-                items: <BottomNavigationBarItem>[
-                  BottomNavigationBarItem(
-                    icon: Icon(Icons.account_balance),
-                    title: const Text(
-                      'Accounts',
-                    ),
-                  ),
-                  BottomNavigationBarItem(
-                    icon: Icon(Icons.add_box),
-                    title: const Text(
-                      'Link',
-                    ),
-                  ),
-                  BottomNavigationBarItem(
-                    icon: Icon(Icons.transfer_within_a_station),
-                    title: const Text(
-                      'Transfer',
-                    ),
-                  ),
-                  BottomNavigationBarItem(
-                    icon: Icon(Icons.person_outline),
-                    title: const Text(
-                      'Profile',
-                    ),
-                  ),
-                ],
-                currentIndex: _.selectedIndex,
-                fixedColor: LightColor.navyBlue2,
-                unselectedItemColor: Colors.black,
-                onTap: _.onItemTapped,
+              BottomNavigationBarItem(
+                icon: Icon(Icons.add_box),
+                title: const Text(
+                  'Link',
+                ),
               ),
-            );
-          },
-        ),
-      ),
+              BottomNavigationBarItem(
+                icon: Icon(Icons.transfer_within_a_station),
+                title: const Text(
+                  'Transfer',
+                ),
+              ),
+              BottomNavigationBarItem(
+                icon: Icon(Icons.person_outline),
+                title: const Text(
+                  'Profile',
+                ),
+              ),
+            ],
+            currentIndex: _.selectedIndex,
+            fixedColor: LightColor.navyBlue2,
+            unselectedItemColor: Colors.black,
+            onTap: _.onItemTapped,
+          ),
+        );
+      },
     );
   }
 }

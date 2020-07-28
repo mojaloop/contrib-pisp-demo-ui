e22590acd8a3b3d36e548678d152098dc2edca67
Kenneth Zeng <kkzeng@edu.uwaterloo.ca>
2020-07-28 15:57:51 +0800

    Replaced shared_pref with secure storage package

--- lib/controllers/ephemeral/user_pin_auth_controller.dart
+++ lib/controllers/ephemeral/user_pin_auth_controller.dart
@@ -1,6 +1,6 @@
 import 'dart:async';
 import 'package:get/get.dart';
-import 'package:shared_preferences/shared_preferences.dart';
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 
 class PINAuthController extends GetxController {
   // Constructor to fetch PIN automatically
@@ -12,6 +12,8 @@ class PINAuthController extends GetxController {
   static const String key = 'USER_PIN';
   static const Duration timeoutDuration = Duration(milliseconds: 1000);
 
+  final FlutterSecureStorage _storage = const FlutterSecureStorage();
+
   // Fields to be populated
   String _correctPIN = '';
   bool _userSetPIN = false;
@@ -20,50 +22,33 @@ class PINAuthController extends GetxController {
   bool get userSetPIN => _userSetPIN;
 
   // Synchronous
-
   // Called after correct PIN has been populated
-  bool authenticate(String pin) {
-    // TODO: Implement actual authentication logic
-    if (pin == _correctPIN) {
-      return true;
-    }
-    return false;
-  }
+  bool authenticate(String pin) => pin == _correctPIN;
 
   Future<void> _fetchPIN() async {
-    final SharedPreferences pref = await Future.any([
-      SharedPreferences.getInstance(),
+    final String pin = await Future.any([
+      _storage.read(key: PINAuthController.key),
       Future.delayed(PINAuthController.timeoutDuration, () => null)
     ]);
 
-    // TODO: Throw some kind of exception
-    if (pref == null) {
-      return;
+    if (pin == null) {
+      print('no previous PIN set');
+      _userSetPIN = false;
     }
-
-    if (pref.containsKey(PINAuthController.key)) {
-      String pin = pref.get(PINAuthController.key).toString();
-      print(pin);
+    else {
+      print('fetched pin as $pin');
       _userSetPIN = true;
       _correctPIN = pin;
-    } else {
-      _userSetPIN = false;
     }
   }
 
   Future<void> storeNewPIN(String pin) async {
     print('storing new pin $pin');
-    final SharedPreferences pref = await Future.any([
-      SharedPreferences.getInstance(),
+    await Future.any([
+      _storage.write(key: PINAuthController.key, value: pin),
       Future.delayed(PINAuthController.timeoutDuration, () => null)
     ]);
 
-    // TODO: Throw some kind of exception
-    if (pref == null) {
-      return;
-    }
-
-    await pref.setString(PINAuthController.key, pin);
     // Update new PIN
     await _fetchPIN();
   }

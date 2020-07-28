24d4ffc4e4f531c0f129f949d86066784c5f541b
Mahidhar <mahi.mahidhar2@gmail.com>
2020-07-28 12:18:50 +0530

    Use bidings in routes. Add new routes for transfer

--- lib/routes/app_routes.dart
+++ lib/routes/app_routes.dart
@@ -5,5 +5,10 @@ abstract class Routes {
   static const LOGIN = '/login';
   static const PROFILE = '/profile';
   static const DASHBOARD = '/dashboard';
+  static const TRANSFER_LOOKUP = '/transfer/lookup';
+  static const TRANSFER_FINALIZE = '/transfer/finalize';
+  static const TRANSGER_DETAILS = 'transfer/details';
+  static const TRANSFER_FAILURE = 'transfer/fail';
+  static const TRANSFER_SUCCESS = 'transfer/success';
 
 }

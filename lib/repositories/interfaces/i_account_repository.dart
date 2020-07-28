ed29b4523bcacfab9d747b87bfde5a690d361440
Mahidhar <mahi.mahidhar2@gmail.com>
2020-07-28 12:27:10 +0530

    Add new method to AccountRepository interface and implement it

--- lib/repositories/interfaces/i_account_repository.dart
+++ lib/repositories/interfaces/i_account_repository.dart
@@ -2,4 +2,5 @@ import 'package:pispapp/models/account.dart';
 
 abstract class IAccountRepository {
   List<Account> getUserAccounts();
+  List<Account> getPayeeAccountsByPhone(String phoneNumber);
 }

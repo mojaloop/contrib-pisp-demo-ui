047e5f405d8aae80e6ef2775760bee364b3cbc18
Mahidhar <mahi.mahidhar2@gmail.com>
2020-08-03 17:27:53 +0530

    Fix routes

--- lib/routes/app_routes.dart
+++ lib/routes/app_routes.dart
@@ -9,7 +9,6 @@ abstract class Routes {
   static const TRANSFER_LOOKUP = '/transfer/lookup';
   static const TRANSFER_FINALIZE = '/transfer/finalize';
   static const TRANSGER_DETAILS = 'transfer/details';
-  static const TRANSFER_FAILURE = 'transfer/fail';
-  static const TRANSFER_SUCCESS = 'transfer/success';
+  static const TRANSFER_VERDICT = 'transfer/verdict';
 
 }

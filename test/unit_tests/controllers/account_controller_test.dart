da9d1ad030fba6eb8b22c5059eda22918cc23cee
Mahidhar <mahi.mahidhar2@gmail.com>
2020-08-07 15:53:10 +0530

    (test): Add AccountController unit tests

---
+++ test/unit_tests/controllers/account_controller_test.dart
@@ -0,0 +1,51 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:get/get.dart';
+import 'package:mockito/mockito.dart';
+import 'package:pispapp/controllers/app/account_controller.dart';
+import 'package:pispapp/controllers/app/auth_controller.dart';
+import 'package:pispapp/models/user.dart';
+import 'package:pispapp/repositories/auth_repository.dart';
+import 'package:pispapp/repositories/interfaces/i_account_repository.dart';
+
+class MockAccountRepository extends Mock implements IAccountRepository {}
+
+class MockAuthRepository extends Mock implements AuthRepository {}
+
+void main() {
+  AuthRepository authRepository;
+  AccountController accountController;
+  IAccountRepository accountRepository;
+  AuthController authController;
+  setUp(
+    () async {
+      authRepository = MockAuthRepository();
+      accountRepository = MockAccountRepository();
+      accountController = AccountController(accountRepository);
+      authController = AuthController(authRepository);
+      Get.put(authController);
+
+      // sign into google
+      when(authRepository.signInWithGoogle()).thenAnswer(
+        (_) => Future.value(
+          User(
+            uid: 'asd67aAhsda768AS',
+            displayName: 'John Doe',
+            email: 'jdoe@example.com',
+          ),
+        ),
+      );
+      await authController.signInWithGoogle();
+    },
+  );
+
+  test(
+    'getAllLinkedAccounts() calls the AccountRepository.getUserAccounts() with signed in user uid',
+    () async {
+      // Call required function
+      await accountController.getAllLinkedAccounts();
+
+      // Check if the repository function was called with right arguments
+      verify(accountRepository.getUserAccounts(authController.user.uid));
+    },
+  );
+}

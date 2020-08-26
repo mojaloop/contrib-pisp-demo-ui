8d0a48e09e06fa5229b7b000ada33ed0f8e07910
Steven Wijaya <stevenwjy149@gmail.com>
2020-08-26 16:02:45 +0800

    Add comments for repository interfaces

--- lib/repositories/interfaces/i_account_repository.dart
+++ lib/repositories/interfaces/i_account_repository.dart
@@ -1,6 +1,7 @@
 import 'package:pispapp/models/account.dart';
 
-// Abstraction for methods related to read/write account information from external data sources
+/// Abstraction for methods related to read/write account
+/// information from external data sources.
 abstract class IAccountRepository {
   /// Returns list of accounts that are associated with a particular user.
   Future<List<Account>> getUserAccounts(String userId);
--- lib/repositories/interfaces/i_auth_repository.dart
+++ lib/repositories/interfaces/i_auth_repository.dart
@@ -1,11 +1,17 @@
 import 'package:pispapp/models/user.dart';
 
+/// Abstraction for methods related to read/write authentication
+/// information from external data sources.
 abstract class IAuthRepository {
-  ///
-  ///
+  /// Prompts the user to sign in using their Google account.
+  /// This function is expected to run asynchronously and return
+  /// a user object that represents the user once they complete
+  /// the sign in process.
   Future<User> signInWithGoogle();
 
-  ///
-  ///
+  /// Signs user out from their account. The execution of this
+  /// function will depend on the login type that was used by the
+  /// user. If the user signed in using their Google account, then
+  /// the execution will also handle the sign out of their Google account.
   Future<void> signOut(User user);
 }

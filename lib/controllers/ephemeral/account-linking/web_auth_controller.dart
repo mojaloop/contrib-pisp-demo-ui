f063fa750d75296deb0e675c976be7a91639cc98
Lewis Daly <lewis@vesselstech.com>
2021-06-04 13:51:21 +0930

    feat: get signed credential for payment flow

--- lib/controllers/ephemeral/account-linking/web_auth_controller.dart
+++ lib/controllers/ephemeral/account-linking/web_auth_controller.dart
@@ -3,11 +3,11 @@ import 'package:get/get.dart';
 class WebAuthController extends GetxController {
   // FSPs must redirect the user to a URI:
   // with pisp as the scheme and token as a queryParam.
-  // i.e. pisp://success?token=1234567
+  // i.e. pisp://success?secret=1234567
   static const customScheme = 'pisp';
 
   String extractAuthTokenFromResult(String result) {
-    final token = Uri.parse(result).queryParameters['token'];
+    final token = Uri.parse(result).queryParameters['secret'];
     return token;
   }
 }
